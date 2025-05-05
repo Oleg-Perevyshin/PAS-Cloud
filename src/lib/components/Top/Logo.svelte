f2df59cd86cc1064b1748279d730d2ef7aaffe45
Oleg Perevyshin <oleg.perevyshin@gmail.com>
2025-05-05 19:43:23 +0300

    Update Logo.svelte

--- src/lib/components/Top/Logo.svelte
+++ src/lib/components/Top/Logo.svelte
@@ -9,5 +9,6 @@
 <div class="mr-auto flex items-center">
   <button class="m-2 flex cursor-pointer items-center justify-center p-2" onclick={navigateHome} type="button">
     <img src="/logo.svg" alt="Logo" class="h-10 w-56" />
+    <!-- <img src="/LogoExample.svg" alt="Logo" class="h-15" /> -->
   </button>
 </div>
